cc883c83593ceb6bed737d8c082ea6ab4436c358
PainOchoco <matteo.planchet@icloud.com>
2023-02-15 12:35:38 +0100

    ⚙️ Restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import MenuButton from "./components/menu/MenuButton";
+import { Satellite, SatelliteRaw } from "./components/scene/satellite";
 import Scene from "./components/scene/Scene";
+import SatellitesContext from "./contexts/SatellitesContext";
+import data from "./data.json";
+
 function App() {
+    const [satellites, setSatellites] = useState(new Array<Satellite>());
+
+    const rawSatellites: SatelliteRaw[] = data as SatelliteRaw[];
+    for (let i = 0; i < rawSatellites.length; i++) {
+        satellites.push(new Satellite(rawSatellites[i]));
+    }
     return (
         <div className="App">
-            <Scene />
+            <SatellitesContext.Provider value={satellites}>
+                <Scene />
+                <MenuButton />
+            </SatellitesContext.Provider>
         </div>
     );
 }
